affc5b7765d39f0dded76900beda5aff542819cc
bval3 <bvalentineii@hotmail.com>
2020-04-24 17:27:05 -0400

    json file

--- server/server.js
+++ server/server.js
@@ -488,7 +488,7 @@ app.post('/api/site/site_data', auth, admin, (req, res) => {
 //DEFAULT
 if( process.env.NODE_ENV === 'production' ){
 //  const path = require('path');
-  app.get('/*',(req,res)=>{
+  app.get('*',(req,res)=>{
       res.sendfile(path.resolve(__dirname,'../client','build','index.html'));
   })
 }
