11f92ca6436cf9f470691158d63fc2a0cc9ce495
bval73 <52051327+bval73@users.noreply.github.com>
2020-04-27 10:17:46 -0400

    change server

--- server/server.js
+++ server/server.js
@@ -47,11 +47,14 @@ const { Site } = require('./models/site')
 const { auth } =require('./middleware/auth');
 const { admin } =require('./middleware/admin');
 
+/*
 app.all('/', function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
   next();
 });
+
+*/
 
 //================================================
 //          PRODUCTS
